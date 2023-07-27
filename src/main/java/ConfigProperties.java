bb3d1c30685523816cbdd47c05ee2e3814f91c48
Metodija747 <metodijabucevski@yahoo.com>
2023-07-27 16:19:25 +0200

    Added configuration parameters

--- src/main/java/ConfigProperties.java
+++ src/main/java/ConfigProperties.java
@@ -5,7 +5,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 @ApplicationScoped
-@ConfigBundle("aws-config")
+@ConfigBundle("config/aws-config")
 public class ConfigProperties {
 
     @ConfigValue(watch = true)
